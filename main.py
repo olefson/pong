257bec81eba8c5614ffa4852f073db309f3865cd
Jason <jasonolefson@gmail.com>
2024-02-19 18:38:46 -0500

    stylized

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ menu_state = main_menu
 # Trackers
 P1_score = 0
 P2_score = 0
+ball_started = False
 
 # Text
 title = pygwidgets.DisplayText(window, (265, 200), "PONG", fontSize=120, textColor=("white"))
@@ -36,6 +37,7 @@ class PlayerPaddle(Paddle):
         super().__init__(window, x, y, width, height, color)
         self.rect = pygame.Rect(x, y, width, height)
         
+        
     def move(self): #moves up and down the screen based on arrow key input
         keys = pygame.key.get_pressed()
         if keys[pygame.K_UP]:
@@ -47,7 +49,11 @@ class PlayerPaddle(Paddle):
         
     
     def handle_event(self, event):
-        pass
+        global ball_started
+        # start moving the ball if the paddle moves
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                ball_started = True
 
 class ComputerPaddle(Paddle):
     def __init__(self, window, x, y, width, height, color):
@@ -92,6 +98,7 @@ class regularBall(Ball):
                 break
             
     def check_collision(self): #checks for collision with walls
+        global ball_started
         global P1_score, P2_score #global variables for score
         if self.y <= 0 or self.y >= WINDOW_HEIGHT:
             self.dy *= -1
@@ -111,12 +118,14 @@ class regularBall(Ball):
             P2ScoreDisplay.setValue("Player 2: " + str(P2_score))
             self.x = WINDOW_WIDTH / 2
             self.y = WINDOW_HEIGHT / 2
+            ball_started = False
             return False
         if RightGoal.rect.collidepoint(self.x, self.y):
             P1_score += 1
             P1ScoreDisplay.setValue("Player 1: " + str(P1_score))
             self.x = WINDOW_WIDTH / 2
             self.y = WINDOW_HEIGHT / 2
+            ball_started = False
             return False
         return False
     
@@ -128,7 +137,8 @@ class regularBall(Ball):
 # Game Elements
 player_paddle = PlayerPaddle(window, 50, 50, 20, 100, ("white"))
 ball = regularBall(window, 400, 300, 10, ("grey"))
-ball.dy = -5
+ball.dy = 0
+ball.dx = -5
 computer_paddle = ComputerPaddle(window, 730, 50, 20, 100, ("blue"))
 # RightWall = GiantRightWall(window, 700, 0, 50, 600, (0, 0, 0))
 LeftGoal = Goal(window, 0, 0, 50, 600, ("black"))
@@ -172,8 +182,10 @@ while run:
         LeftGoal.draw()
         RightGoal.draw()
         
-        ball.move()
+        if ball_started:
+            ball.move()
         ball.draw()
+        
         P1ScoreDisplay.draw()
         P2ScoreDisplay.draw()
         if P1_score >= 5 or P2_score >= 5:
@@ -184,6 +196,7 @@ while run:
             if event.type == pygame.QUIT:
                 run = False
             ball.handle_event(event)
+            player_paddle.handle_event(event)
             
     
     if menu_state == game_over:
